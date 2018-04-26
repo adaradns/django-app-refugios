2529040a089c999358309c9950b6ad9a66408c6c
adaradenis@gmail.com <adaradenis@gmail.com>
2018-04-25 21:09:51 -0300

    Routing (urls) de la app

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,10 +1,9 @@
 
-
-from apps.mascota.views import index
+from apps.adopcion.views import index_adopcion
 from django.urls import path
 from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', index),
+	url(r'^index$', index_adopcion)
 ]
--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("Index")
+def index_adopcion(request):
+	return HttpResponse("Pagina principal Adopcion")
 
